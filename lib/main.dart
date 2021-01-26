8a13ec1b34c940f1b6efb4a09534ba53edb58569
Samip <samipgnyawali15@gmail.com>
2021-01-26 19:59:17 +0545

    Adding Native Code

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Native code',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _batteryLevel;
+  // get battery level from ios and android platform
+  Future<void> _getBatteryLevel() async {
+    const platform =
+        MethodChannel("samples.flutter.dev/battery"); // unique domain prefix
+    try {
+      final batteryLevel = await platform.invokeMethod('getBatteryLevel');
+      setState(() {
+        _batteryLevel = batteryLevel;
+      });
+    } on PlatformException catch (error) {
+      _batteryLevel = null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getBatteryLevel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Battery"),
+      ),
+      body: Center(
+        child: Text("Battery Level $_batteryLevel"),
+      ),
+    );
+  }
+}
